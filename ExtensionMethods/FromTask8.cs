e2855743b2343a441c4873606738b6035ebd1cc1
Jivan <jivan241003@gmail.com>
2023-07-04 23:19:48 +0400

    Task 9: Data Conversion

--- ExtensionMethods/FromTask8.cs
+++ ExtensionMethods/FromTask8.cs
@@ -1,9 +1,21 @@
 ﻿using System;
 
+public enum Color
+{
+    Red, Yellow, White
+}
 public static class ThirdExtensionClass
 {
     public static bool FileExists(this string str)
     {
         return File.Exists(str);
     }
+
+    public static T toEnum<T>(this string str) where T:struct{
+        T enu;
+        Enum.TryParse<T>(str, true, out enu);
+        return enu;
+    }
+
+
 }
