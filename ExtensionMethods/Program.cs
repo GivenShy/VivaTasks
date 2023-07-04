920616df67c8c44af14087c09e051638f1793370
Jivan <jivan241003@gmail.com>
2023-07-04 15:44:08 +0400

    Task 1: String Manipulation

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+public static class ExtensionClass
+{
+    public static string ToTitleCase(this string str)
+    {
+        string[] newString = str.Split(' ');
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (string s in newString)
+        {
+            stringBuilder.Append(s[0].ToString().ToUpper());
+            stringBuilder.Append(s.Substring(1));
+            stringBuilder.Append(" ");
+        }
+        string s = stringBuilder.ToString();
+        s = s.Trim();
+        return s;
+    }
+}
 
