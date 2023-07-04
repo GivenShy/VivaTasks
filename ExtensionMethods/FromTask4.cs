3b1d9182bd13465055cd5c936990eae42d4385a4
Jivan <jivan241003@gmail.com>
2023-07-04 21:10:36 +0400

    Task 4: Validation

---
+++ ExtensionMethods/FromTask4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public static class SecondExtentionClass
+{
+    public static bool IsValidEmail(this string str)
+    {
+        string[] strings = str.Split("@");
+        if (strings.Length != 2)
+        {
+            return false;
+        }
+        string s = strings[0];
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!Char.IsLetterOrDigit(s[i])){
+                if (s[i]!='-' || s[i] != '_' || s[i] != '.' || s[i] != '-')
+                {
+                    return false;
+                }
+            }
+        }
+        string[] st = strings[1].Split(".");
+        if (st.Length < 2)
+        {
+            return false;
+        }
+        for(int i = 0; i < st.Length; i++)
+        {
+            for(int j = 0; j < st[i].Length; j++)
+            {
+                if (!Char.IsLetterOrDigit(s[i]))
+                {
+                    if (s[i] != '-' || s[i] != '_' || s[i] != '-')
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+
+    }
+}
