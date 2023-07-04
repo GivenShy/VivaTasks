9581d6cdf16d4f06d42012fdfc1ec4ed23f78bad
Jivan <jivan241003@gmail.com>
2023-07-04 22:35:22 +0400

    Task 8: File Handling

---
+++ ExtensionMethods/FromTask8.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+Console.ReadKey();
+public static class ThirdExtensionClass
+{
+    public static bool FileExists(this string str)
+    {
+        return File.Exists(str);
+    }
+}
