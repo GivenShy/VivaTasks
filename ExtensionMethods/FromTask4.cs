9ebaffbb7770a783b570f3012a39bcbe29a45a2a
Jivan <jivan241003@gmail.com>
2023-07-04 22:28:14 +0400

    Task 7: Number Rounding

--- ExtensionMethods/FromTask4.cs
+++ ExtensionMethods/FromTask4.cs
@@ -2,11 +2,6 @@
 using System.Text;
 using System.Text.Json;
 
-
-
-Console.WriteLine(new Person(23, "Hello").SerializeToJson());
-Console.ReadKey();
-
 public static class SecondExtentionClass
 {
     public static bool IsValidEmail(this string str)
@@ -78,6 +73,30 @@ public static class SecondExtentionClass
     {
         return JsonSerializer.Serialize<T>(obj, new JsonSerializerOptions() { WriteIndented = true });
     }
+    public static double Round(this double number, int i)
+    {
+        double num = Math.Truncate(number);
+        double dec = number - num;
+        string str = dec.ToString().Split(".")[1];
+        string t="";
+        if (str[i] - '0' < 5)
+        {
+            if (i == 0)
+            {
+                return num;
+            }
+            t = "0." + str.Substring(0, i);
+            return num + Double.Parse(t);
+        }
+        else
+        {
+            if (i == 0)
+                return num + 1;
+            t = "0." + str.Substring(0, i - 1) + (char)(str[i-1] + 1);
+            return num + Double.Parse(t);
+        }
+    }
+    
 }
 
 
