6af0967f996da83f09a67035843cf14778021e9d
Jivan <jivan241003@gmail.com>
2023-07-04 20:27:18 +0400

    Collection Sorting

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -1,4 +1,29 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
+
+public class Person
+{
+    public Person(int a)
+    {
+        age = a;
+    }
+    private int age;
+    public int Age
+    {
+        get
+        {
+            return age;
+        }
+        set
+        {
+            age = value;
+        }
+    }
+    public override string ToString()
+    {
+        return age.ToString();
+    }
+}
 
 public static class ExtensionClass
 {
@@ -12,9 +37,57 @@ public static class ExtensionClass
             stringBuilder.Append(s.Substring(1));
             stringBuilder.Append(" ");
         }
-        string s = stringBuilder.ToString();
-        s = s.Trim();
-        return s;
+        string st = stringBuilder.ToString();
+        st = st.Trim();
+        return st;
+    }
+
+    public static List<T> SortByProperty<T>(this List<T> list, Func<T, IComparable> func)
+    {
+        List<T> newList = new List<T>(list);
+        sortByQuickSort(newList, func, 0, newList.Count()-1);
+        return newList;
+
+    }
+
+    private static void sortByQuickSort<T>(List<T> list, Func<T,IComparable> func,int left, int right)
+    {
+        if (left >= right)
+        {
+            return;
+        }
+        var i = left;
+        var j = right-1;
+        var pivot = list[right];
+        T temp;
+        while (i < j)
+        {
+            while (i<=j && func(list[i]).CompareTo(func(pivot))<0)
+            {
+                i++;
+
+            }
+
+            while (j>left & i<right-1 && i<=j && func(list[j]).CompareTo(func(pivot)) > 0)
+            {
+                j--;
+            }
+            if (i < j)
+            {
+                
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+                i++;
+                j--;
+
+            }
+        }
+        temp = list[i];
+        list[i] = pivot;
+        list[right] = temp;
+        sortByQuickSort(list, func, left, i - 1);
+        sortByQuickSort(list, func, i + 1, right);
     }
 }
 
