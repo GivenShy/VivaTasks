45238f10260fc3a13ef9b7ab10417f36b17bbd50
Jivan <jivan241003@gmail.com>
2023-07-06 14:05:51 +0400

    Task 10: LINQ Integration and Task 11: Call Data Analysis

--- ExtensionMethods/FromTask8.cs
+++ ExtensionMethods/FromTask8.cs
@@ -4,6 +4,12 @@ public enum Color
 {
     Red, Yellow, White
 }
+
+public class CallRecords
+{
+    public DateTime startTime { get; set; }
+    public DateTime endTime { get; set; }
+}
 public static class ThirdExtensionClass
 {
     public static bool FileExists(this string str)
@@ -17,5 +23,27 @@ public static class ThirdExtensionClass
         return enu;
     }
 
+    public static IEnumerable<T> filter<T>(this IEnumerable<T> values,bool condition,Func<T,bool> predicate)
+    {
+        if (condition)
+        {
+            return values.Where(predicate);
+        }
+        return values;
+    }
+
+    public static double CalculateTotalDuration(this IEnumerable<CallRecords> calls)
+    {
+        DateTime zero = new DateTime(1970, 1, 1);
+        double totalDuration = 0;
+
+        foreach(CallRecords call in calls)
+        {
+            TimeSpan time1 = call.startTime.Subtract(zero);
+            TimeSpan time2 = call.endTime.Subtract(zero);
+            totalDuration += time2.TotalMilliseconds - time2.TotalMilliseconds;
+        }
+        return totalDuration;
+    }
 
 }
