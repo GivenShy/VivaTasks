f60258ba6fd20ebb36cce6f3f72fa1a496166e35
Jivan <jivan241003@gmail.com>
2023-07-07 15:04:30 +0400

    Task 3: Anonymous Method

--- Delegates/Main.cs
+++ Delegates/Main.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 namespace Delegates
 {
-	public class MainClass
-	{
-		public static void Main()
-		{
-			Button button = new Button();
-			button.ConsoleReadEvent += () => Console.WriteLine("The button that I created was clicked");
-			button.Click();
-			Console.ReadKey();
-		}
-	}
+    public class MainClass
+    {
+        public static void Main()
+        {
+            //Button button = new Button();
+            //button.ConsoleReadEvent += () => Console.WriteLine("The button that I created was clicked");
+            //button.Click();
+            OperationDelegate operationDelegate;
+            operationDelegate = delegate (int first, int second)
+            {
+                int result = first + second;
+                Console.WriteLine(result);
+                return result;
+            };
+            operationDelegate += delegate (int first, int second)
+            {
+                int result = first * second;
+                Console.WriteLine(result);
+                return result;
+
+            };
+
+            Console.WriteLine(operationDelegate.Invoke(3, 4));
+            Console.ReadKey();
+        }
+    }
+
+    public delegate int OperationDelegate(int first, int second);
 }
 
