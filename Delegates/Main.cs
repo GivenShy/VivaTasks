ec54beb634e5474bcd4c28629f92661135f318e2
Jivan <jivan241003@gmail.com>
2023-07-07 15:08:54 +0400

    Task 4: Lambda Expression

--- Delegates/Main.cs
+++ Delegates/Main.cs
@@ -8,26 +8,40 @@ namespace Delegates
             //Button button = new Button();
             //button.ConsoleReadEvent += () => Console.WriteLine("The button that I created was clicked");
             //button.Click();
-            OperationDelegate operationDelegate;
-            operationDelegate = delegate (int first, int second)
-            {
-                int result = first + second;
-                Console.WriteLine(result);
-                return result;
-            };
-            operationDelegate += delegate (int first, int second)
-            {
-                int result = first * second;
-                Console.WriteLine(result);
-                return result;
+            //
+            //OperationDelegate operationDelegate;
+            //operationDelegate = delegate (int first, int second)
+            //{
+            //    int result = first + second;
+            //    Console.WriteLine(result);
+            //    return result;
+            //};
+            //operationDelegate += delegate (int first, int second)
+            //{
+            //    int result = first * second;
+            //    Console.WriteLine(result);
+            //    return result;
 
+            //};
+
+            //Console.WriteLine(operationDelegate.Invoke(3, 4));
+
+            ComparisonDelegate comparison = (firstNumber, secondNumber) =>
+            {
+                if (firstNumber > secondNumber)
+                    return 1;
+                if (firstNumber == secondNumber)
+                    return 0;
+                return -1;
             };
 
-            Console.WriteLine(operationDelegate.Invoke(3, 4));
+            Console.WriteLine(comparison(1, 3));
             Console.ReadKey();
         }
     }
 
     public delegate int OperationDelegate(int first, int second);
+
+    public delegate int ComparisonDelegate(int firstNumber, int secondNumber);
 }
 
