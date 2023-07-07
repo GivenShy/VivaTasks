51b1ac2f014d46ab0641f9cb685a7c0e4ec79df4
Jivan <jivan241003@gmail.com>
2023-07-07 14:04:55 +0400

    Task 1: Delegate Invocation

---
+++ Delegates/Task1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace delegates
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Print print;
+            print = ConsolePrint;
+            print += delegate
+            {
+                Console.WriteLine("Hello from anonymous class");
+            };
+            print("Hello");
+            Console.ReadKey();
+           
+        }
+        static void ConsolePrint(string str)
+        {
+            Console.WriteLine(str);
+        }
+        static void OtherPrint()
+        {
+            Console.WriteLine("HelloWorld");
+        }
+    }
+
+    public delegate void Print(string str);
+}
