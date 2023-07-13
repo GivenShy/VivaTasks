0e7dcace78b852b6c449be391aa04d417c54822a
Jivan <jivan241003@gmail.com>
2023-07-13 16:12:00 +0400

    Task 1. Write a program which reads lines in-parallel from two files, during which every one second it prints in console the elapsed seconds and at the end it prints in the console the sum of lines of two documents together.

---
+++ Multithreading/MainClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace Multithreading
+{
+    public class MainClass
+    {
+        public MainClass()
+        {
+        }
+
+        public static void Main()
+        {
+            PrintFromFiles("/Users/jivanshmavonyan/Desktop/Tasks/ExceptionHandling/text.txt",
+                "/Users/jivanshmavonyan/Desktop/Tasks/ExceptionHandling/t.txt");
+            Console.ReadKey();
+        }
+
+        public static void PrintFromFiles(string path1, string path2)
+        {
+            int elapsedSeconds = 0;
+            Timer timer = new Timer(state =>
+            {
+                elapsedSeconds++;
+                Console.WriteLine("Elapsed seconds: " + elapsedSeconds);
+            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+            Task<int> task1 = new Task<int>(() => readLineByLine(path1));
+            Task<int> task2 = new Task<int>(() => readLineByLine(path2));
+            task1.Start();
+            Console.WriteLine("started");
+            task2.Start();
+            Console.WriteLine("started");
+            task1.Wait();
+            task2.Wait();
+
+            int sum = task1.Result + task2.Result;
+            Console.WriteLine(sum);
+            timer.Dispose();
+        }
+
+
+
+        public static int readLineByLine(string path)
+        {
+            int counter = 0;
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+    }
+}
+
